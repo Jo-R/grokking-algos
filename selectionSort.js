a0fe5b1539d8e96cf2a988cd67c1574189a41875
Jo-R <jo.rigg8@gmail.com>
2020-05-03 14:55:59 +0100

    selection sort ch2

---
+++ selectionSort.js
@@ -0,0 +1,28 @@
+function findIndexOfSmallest(array) {
+  let smallest = array[0];
+  smallestIdx = 0;
+
+  array.forEach((el, i) => {
+    if (el < smallest) {
+      smallest = el;
+      smallestIdx = i;
+    }
+  });
+
+  return smallestIdx;
+}
+
+function selectionSort(array) {
+  let sortedArray = [];
+  let smallestIdx;
+  const arrayLength = array.length;
+  for (let i = 0; i < arrayLength; i++) {
+    smallestIdx = findIndexOfSmallest(array);
+    sortedArray.push(array[smallestIdx]);
+    array.splice(smallestIdx, 1);
+  }
+
+  return sortedArray;
+}
+
+console.log(selectionSort([5, 3, 6, 2, 10]));
