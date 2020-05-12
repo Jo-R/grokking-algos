24c4d4283df238f2357e6186b8b2852e08259a48
Jo-R <jo.rigg8@gmail.com>
2020-05-12 17:24:46 +0100

    ch3 recursion

---
+++ recursion.js
@@ -0,0 +1,24 @@
+// function countdown(x) {
+//   console.log(x);
+//   if (x <= 1) {
+//     // base case
+//     return;
+//   } else {
+//     countdown(x - 1); //recursive case
+//   }
+// }
+
+// countdown(10);
+
+function factorial(x) {
+  if (x === 1) {
+    console.log("returning 1");
+    return 1; // this is first thing returned then the previous calls down the stack...
+  } else {
+    const num = x * factorial(x - 1); // these calls go on the stack
+    console.log(`${num} for ${x}`);
+    return num;
+  }
+}
+
+console.log(factorial(3));
